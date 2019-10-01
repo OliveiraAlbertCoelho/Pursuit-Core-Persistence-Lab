ef10e15f659a86511658a65b6cac53ca27c85aab
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-09-30 22:43:08 -0400

    setting up viewControllers and design

---
+++ PersistenceLab/PersistenceLab/CustomCell/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PersistenceLab
+//
+//  Created by albert coelho oliveira on 9/30/19.
+//  Copyright © 2019 albert coelho oliveira. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImageCell: UIView!
+}
---
+++ PersistenceLab/PersistenceLab/Persistence/PhotoPersistenceHelper.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoPersistenceHelper.swift
+//  PersistenceLab
+//
+//  Created by albert coelho oliveira on 9/30/19.
+//  Copyright © 2019 albert coelho oliveira. All rights reserved.
+//
+
+import Foundation
